babf98c2c1d4a798ca14987941531ccb2d6f0a31
Daniele Olmisani <daniele.olmisani@gmail.com>
2025-08-22 07:16:02 +0200

    Create anno_meno_pioggia.sql

---
+++ anno_meno_pioggia.sql
@@ -0,0 +1,10 @@
+SELECT 
+    substr(time, 1, 4) AS anno, 
+    SUM(precipitation_sum) AS totale_precipitazioni
+FROM 
+    daily
+GROUP BY 
+    anno
+ORDER BY 
+    totale_precipitazioni ASC
+LIMIT 10;
