06c1e4295e6735f0818792379e85da1ec0d27ffa
Daniele Olmisani <daniele.olmisani@gmail.com>
2025-08-22 09:49:37 +0200

    Update

--- sql/diagram_precipitations.sql
+++ sql/diagram_precipitations.sql
@@ -1,9 +1,10 @@
 .headers on
 .mode json
-.output precipitations_per_year.json
+.output ../precipitations_per_year.json
 SELECT 
     substr(time, 1, 4) AS anno,
-    ROUND(SUM(precipitation_sum), 0) AS totale_precipitazioni
+    CAST(ROUND(SUM(precipitation_sum), 0) AS INTEGER) AS totale_precipitazioni,
+    CAST(ROUND(SUM(snowfall_sum), 1) AS INTEGER) AS totale_neve
 FROM 
     daily
 GROUP BY 
